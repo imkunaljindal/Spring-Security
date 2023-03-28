328c08eef988cdf007795954ff736582bf48865a
Kunal Jindal <kunaljindal@mnnit.ac.in>
2023-03-28 20:54:12 +0530

    Security Lecture

---
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails student = User.withUsername("student1")
+                .password(getPasswordEncoder().encode("student"))
+                .roles("STUDENT")
+                .build();
+
+        UserDetails teacher = User.withUsername("teacher1")
+                .password(getPasswordEncoder().encode("teacher"))
+                .roles("TEACHER")
+                .build();
+
+        return new InMemoryUserDetailsManager(student,teacher);
+
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/visitor/**")
+                .permitAll()
+                .requestMatchers("/student/**")
+                .hasRole("STUDENT")
+                .requestMatchers("/teacher/**")
+                .hasRole("TEACHER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+
+        return http.build();
+    }
+}
